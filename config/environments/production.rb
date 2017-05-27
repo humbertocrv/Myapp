61e574d7c08c421769024d47388ce76929c95c69
Humberto Ramos <humbertocrv@hotmail.com>
2017-05-27 16:39:27 -0500

    archivo config.ru modificado y config production.rb con linea de imagenes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -67,8 +69,7 @@ Rails.application.configure do
    #   enable_starttls_auto: true,
    #   user_name: "ingeniero070385@gmail.com",
    #   password: "Rave123*" 
-
-  #}
+   #}
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
